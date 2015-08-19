24c0dcd5169b0a56c956c4c49372c1d325177eed
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2015-08-19 22:34:03 +0800

    import books.view

--- BookEx/BookEx/urls.py
+++ BookEx/BookEx/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from books.views import book
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
