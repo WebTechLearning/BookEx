ec6ed7618c866c1e99dced674a0a8411ddd4f7ea
tensan <tensan.ee96@g2.nctu.edu.tw>
2015-08-20 00:50:47 +0800

    Add models.py

--- BookEx/books/models.py
+++ BookEx/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class book(object):
+	title = models.CharField(max_length=20)
+
+ 	"""docstring for book"""
+ 	def __unicode__(self):
+ 		return self.title
 # Create your models here.
