ffc5d0ee2169bb10dcbedfdaa89c35a762e2d49c
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2015-08-19 22:00:51 +0800

    add view, book

--- BookEx/books/views.py
+++ BookEx/books/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from book.models import Book
 
 # Create your views here.
+def book(request):
+    books = Books.objects.order_by('create_date')
+    return render_to_response('books.html', locals())
