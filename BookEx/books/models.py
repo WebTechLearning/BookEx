477284caa6c0c7df7213e3c5fa5aff3ca83f8bec
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2015-08-26 21:33:37 +0800

    update model

--- BookEx/books/models.py
+++ BookEx/books/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-	title = models.CharField(max_length=20)
-
- 	"""docstring for book"""
- 	def __unicode__(self):
- 		return self.title
+    title = models.CharField(max_length=64)
+    paperback = models.CharField(max_length=5)
+    publisher = models.CharField(max_length=64)
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=13, blank=True)
+    create_date = models.DateField()
+    publish_date = models.DateField()
+    comment = models.TextField(blank=True)
+    cover = models.ImageField(upload_to='image', blank=True)
+    
+    """docstring for book"""
+    def __unicode__(self):
+        return self.title
