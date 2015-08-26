b7b6fe12f7ad0cb19317d540db30044ddbe74a9e
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2015-08-26 20:58:47 +0800

    minor

--- BookEx/books/models.py
+++ BookEx/books/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
-class Book(object):
+# Create your models here.
+class Book(models.Model):
 	title = models.CharField(max_length=20)
 
  	"""docstring for book"""
  	def __unicode__(self):
  		return self.title
-# Create your models here.
